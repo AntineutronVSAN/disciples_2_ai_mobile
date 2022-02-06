f13b274f725bb92460f84bf0be3aff91f2396f8a
Andrey <schabalin.andrej2014@yandex.ru>
2022-02-06 14:02:51 +0300

    add firebase

---
+++ lib/services/firebase.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class FirebaseServices {
+
+  static void initFirebaseApp() async {
+
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+
+  }
+
+}
